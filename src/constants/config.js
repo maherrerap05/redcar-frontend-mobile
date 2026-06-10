f048e28a011102dfc979bbde0b0e620ab8d416ff
maherrerap05 <maherrerap@puce.edu.ec>
2026-06-09 23:03:55 -0500

    config: actualizar URL Gateway a produccion Oracle Cloud

--- src/constants/config.js
+++ src/constants/config.js
@@ -3,7 +3,7 @@
 // URL pública del Marketplace Event Gateway expuesta via ngrok.
 // IMPORTANTE: ngrok genera una URL nueva cada vez que se reinicia.
 // Si reinicias ngrok, actualiza esta URL y reinicia Expo.
-export const GATEWAY_GRAPHQL_URL = 'https://follow-bottle-daintily.ngrok-free.dev/graphql';
+export const GATEWAY_GRAPHQL_URL = 'https://redcar-api.duckdns.org/gateway/graphql';
 
 // IVA aplicado en Ecuador
 export const IVA_PORCENTAJE = 0.15;
